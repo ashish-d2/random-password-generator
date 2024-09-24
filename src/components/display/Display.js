bb70bad9f89a143833e21381558c9c30f30b344c
Ashish Dungdung <official.ashishd2@gmail.com>
2024-09-24 21:27:54 +0530

    Implemented copy-btn click style

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,11 +1,22 @@
 import styles from "./Display.module.scss";
 import { ReactComponent as CopyIcon } from "./../../assets/images/icon-copy.svg";
+import { useState } from "react";
 
 export default function Display() {
+  const [copyBtnActive, setCopyBtnActive] = useState(false);
+
+  const handleCopyBtnClick = function () {
+    setCopyBtnActive(true);
+  };
+
   return (
     <div className={styles.displayContainer}>
       <p className={`heading-medium ${styles.empty}`}></p>
-      <CopyIcon className={styles.copyIcon} />
+      <div className={styles.copyContainer}>
+        {copyBtnActive ? <p className={styles.copied}>Copied</p> : ""}
+
+        <CopyIcon className={styles.copyIcon} onClick={handleCopyBtnClick} />
+      </div>
     </div>
   );
 }
